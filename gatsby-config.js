551d07ec26b4d6e1f1b4554a6fd848aee19b3d4a
Sean Wu <seanwuapps@gmail.com>
2021-05-22 21:12:05 +1000

    gtag replace ga plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,13 +5,34 @@ module.exports = {
   plugins: [
     'gatsby-plugin-sass',
     'gatsby-plugin-image',
+    'gatsby-plugin-react-helmet',
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: '269428730',
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          '269428730', // Google Analytics / GA
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: 'OPT_CONTAINER_ID',
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          // exclude: [],
+        },
       },
     },
-    'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
